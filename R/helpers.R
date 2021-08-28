16bf08f97dd318c88be95fee42c0082078417f50
Maximilian Held <info@maxheld.de>
2021-08-28 20:36:11 +0200

    assert email

--- R/helpers.R
+++ R/helpers.R
@@ -2,6 +2,7 @@
 #' @export
 #' @keywords internal
 is_email_address <- function(x) {
+  stopifnot(rlang::is_scalar_character(x))
   grepl(
     "^\\s*[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\s*$",
     as.character(x),
